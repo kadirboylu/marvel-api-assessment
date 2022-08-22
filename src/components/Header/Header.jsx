b493d5173f864facd6efb12133b29a6e34ca732d
kadirboylu <kadir.boylu@outlook.com>
2022-08-22 12:02:16 +0300

    Header section designed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import logo from "@/assets/img/marvel-logo.png";
+import styles from "./style.module.scss";
+import { GoSearch } from "react-icons/go";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <img className={styles.logo} src={logo} alt="marvel-logo" />
+      <div className={styles.search}>
+        <GoSearch className={styles["search-icon"]} />
+        <input
+          className={styles["search-bar"]}
+          type="text"
+          placeholder="Search heroes"
+        />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
