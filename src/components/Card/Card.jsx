e66e2a51d7176a11d33ff3d5e30ba14824c09ac7
kadirboylu <kadir.boylu@outlook.com>
2022-08-24 20:48:36 +0300

    Add details

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,20 +2,22 @@ import React from "react";
 import styles from "./style.module.scss";
 import { CgArrowLongRight } from "react-icons/cg";
 
-const Card = () => {
+const Card = ({ character }) => {
   return (
     <div className={`${styles.base} ${styles["slide-in"]}`}>
-      <img src="https://picsum.photos/200/200" alt="hero-img" />
+      <img
+        src={`${character.thumbnail.path}/portrait_xlarge.${character.thumbnail.extension}`}
+        alt="character-img"
+      />
       <div className={styles.heroCaption}>
-        <h2>HERO NAME</h2>
+        <div className={styles.title}>
+          <h2>{character.name}</h2>
+        </div>
         <div className={styles["info-wrapper"]}>
           <p className={styles.info}>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio
-            sapiente libero vel ipsum, repellendus eaque sunt? Doloribus impedit
-            distinctio voluptate dolorem dolor quod maiores inventore nesciunt
-            quas facere. Odit, voluptates impedit quaerat consectetur delectus
-            minus et, laboriosam voluptatum dolorem recusandae voluptas at.
-            Dolor maiores, reiciendis assumenda atque nobis earum architecto.
+            {character.description === ""
+              ? "No description given."
+              : character.description}
           </p>
           <a href="#">
             Click to see more
