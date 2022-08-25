37d3e370af396d28ce1238749423f25bacb395a0
kadirboylu <kadir.boylu@outlook.com>
2022-08-25 21:24:50 +0300

    Add details

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import charactersReducer from "@/slices/characters/charactersSlice";
+import detailsReducer from "@/slices/details/detailsSlice";
 
 export const store = configureStore({
-  reducer: { characters: charactersReducer },
+  reducer: { characters: charactersReducer, details: detailsReducer },
 });
