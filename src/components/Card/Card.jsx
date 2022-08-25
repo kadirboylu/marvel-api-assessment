fc8e60b81dea33a2caeb15ed973694e7ea61c624
kadirboylu <kadir.boylu@outlook.com>
2022-08-25 21:26:17 +0300

    Change images

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,13 +2,21 @@ import React from "react";
 import styles from "./style.module.scss";
 import { Link } from "react-router-dom";
 import { CgArrowLongRight } from "react-icons/cg";
+import { useDispatch } from "react-redux";
+import { setCharacter } from "@/slices/details/detailsSlice";
 
 const Card = ({ character }) => {
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(setCharacter(character));
+  };
+
   return (
     <div className={`${styles.base} ${styles["slide-in"]}`}>
       {/* Character Img */}
       <img
-        src={`${character.thumbnail.path}/portrait_incredible.${character.thumbnail.extension}`}
+        src={`${character.thumbnail.path}/standard_incredible.${character.thumbnail.extension}`}
         alt="character-img"
       />
       <div className={styles.heroCaption}>
@@ -19,12 +27,12 @@ const Card = ({ character }) => {
         <div className={styles["info-wrapper"]}>
           {/* Description */}
           <p className={styles.info}>
-            {character.description === ""
+            {character.description === "" || character.description === null
               ? "No description given."
               : character.description}
           </p>
           {/* Click to see more */}
-          <Link to="/character">
+          <Link onClick={handleClick} to="/character">
             Click to see more
             <CgArrowLongRight className={styles.icon} />
           </Link>
