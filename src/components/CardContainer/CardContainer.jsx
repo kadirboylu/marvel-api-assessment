989a1d9bd90da53a7df4b138f4ae32658d7f485e
kadirboylu <kadir.boylu@outlook.com>
2022-08-25 13:56:20 +0300

    Add infinite scroll

--- src/components/CardContainer/CardContainer.jsx
+++ src/components/CardContainer/CardContainer.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect } from "react";
 import styles from "./style.module.scss";
-import Card from "@/components/Card";
 import { useSelector, useDispatch } from "react-redux";
 import { getCharacters, loadMore } from "@/slices/characters/charactersSlice";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Card from "@/components/Card";
+import Loading from "../Loading/Loading";
 
 const CardContainer = () => {
   const dispatch = useDispatch();
@@ -14,11 +16,19 @@ const CardContainer = () => {
   }, [offset]);
 
   return (
-    <div className={styles.container}>
-      {characters.map((character, index) => {
-        return <Card key={index} character={character} />;
-      })}
-    </div>
+    <InfiniteScroll
+      dataLength={characters.length}
+      hasMore={true}
+      next={() => dispatch(loadMore(offset))}
+      loader={<Loading />}
+      pullDownToRefreshThreshold={1000}
+    >
+      <div className={styles.container}>
+        {characters.map((character, index) => {
+          return <Card key={index} character={character} />;
+        })}
+      </div>
+    </InfiniteScroll>
   );
 };
 
